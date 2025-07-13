b2ef078dbb5debf8ad4198700207f6308e514255
mpwoz <martin.woz@gmail.com>
2025-07-12 18:16:06 -0700

    v1

---
+++ Source/StrongPawns/StrongPawns.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using Verse;
+
+namespace StrongPawns
+{
+    using System.Reflection;
+    using HarmonyLib;
+
+    [StaticConstructorOnStartup]
+    public class StrongPawns
+    {
+        static StrongPawns()
+        {
+            var harmony = new Harmony("mpwoz.StrongPawns");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("StrongPawns patch applied.");
+        }
+
+        public Harmony Harmony { get; set; }
+    }
+
+    [HarmonyPatch(typeof(MassUtility), nameof(MassUtility.Capacity))]
+    public static class HarmonyPatches
+    {
+        public static void Postfix(ref float __result)
+        {
+            const float multiplier = 20f;
+            __result *= multiplier;
+        }
+    }
+}
