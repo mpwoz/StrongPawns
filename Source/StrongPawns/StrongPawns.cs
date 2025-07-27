2d40955ab29da81e3b3ef0818bb0f9b1a2ba31d3
mpwoz <martin.woz@gmail.com>
2025-07-27 09:43:41 -0700

    fix: set carry capacity to a constant value, rather than multiplying

--- Source/StrongPawns/StrongPawns.cs
+++ Source/StrongPawns/StrongPawns.cs
@@ -3,6 +3,7 @@ using Verse;
 
 namespace StrongPawns
 {
+    using System;
     using System.Reflection;
     using HarmonyLib;
 
@@ -24,8 +25,11 @@ namespace StrongPawns
     {
         public static void Postfix(ref float __result)
         {
-            const float multiplier = 20f;
-            __result *= multiplier;
+            // context: we used to try to multiply the value here to factor in pawn size, etc. but that leads to display
+            // issues, probably because this method gets called twice so we end up double multiplying (getting something
+            // insane like 14000 instead of the desired ~700). So instead, just hardcode the value to 750 and call it a
+            // day (unless it's zero, e.g. pawn incapacitated, etc.)
+            if (__result > 0f) { __result = 750f; }
         }
     }
 }
--- Source/StrongPawns/obj/Debug/StrongPawns.AssemblyInfo.cs
+++ Source/StrongPawns/obj/Debug/StrongPawns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StrongPawns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ef078dbb5debf8ad4198700207f6308e514255")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7155755b2396254d8c262aa27ed627f0142fb37")]
 [assembly: System.Reflection.AssemblyProductAttribute("StrongPawns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StrongPawns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
